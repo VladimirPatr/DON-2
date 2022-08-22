8be122875e9890840b016d506bca0a25150bd07c
Vladimir Patrikeyev <patrik666@gmail.com>
2022-08-22 19:08:49 +0300

    dom-2-js

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const modalTitle = document.querySelector('.title'); //заголовок
+const modalID = document.querySelector('.edit-wrapper span'); //ID
+const modalIdButton = document.querySelector('.btn-edit'); //кнопка ID
+const modalForm = document.querySelector('.form'); //форма
+const modalCheckbox = document.querySelector('.form__input_chk'); //чекбокс
+const modalCheckboxInput = document.querySelector('.form__input_sale'); //input с чекбоксом
+const modalTotalCost = document.querySelector('.form-footer span'); //итоговая стоимость
